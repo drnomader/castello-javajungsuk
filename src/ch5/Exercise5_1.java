863d89c40f0330f5e30f81eda3b44e1a1f5c11df
drnomader <drnomader@gmail.com>
2025-03-17 22:27:05 +0900

    Added ex5_1-ex5_5

---
+++ src/ch5/Exercise5_1.java
@@ -0,0 +1,10 @@
+package ch5;
+
+public class Exercise5_1 {
+	int[] arr1[];
+	int[] arr2 = {1, 2, 3,};
+	int[] arr3 = new int[5];
+//	int[] arr4 = new int[5]{1,2,3,4,5}; // 두 번째 대괄호[]에 숫자 넣으면 안 됨
+//	int arr5[5]; // 배열 선언 시 배열 크기 지정 불가
+	int[] arr6[] = new int[3][];
+}
