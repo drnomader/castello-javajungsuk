a8d34ba4b32e3ed85e30729937715f97ecf7616d
drnomader <drnomader@gmail.com>
2025-03-25 23:24:34 +0900

    Added 6_3

---
+++ src/ch6/Exercise6_4.java
@@ -0,0 +1,31 @@
+package ch6;
+
+public class Exercise6_4 {
+	public static void main(String args[]) {
+		Student s = new Student();
+		s.name = " ";
+		s.ban = 1;
+		s.no = 1;
+		s.kor = 100;
+		s.eng = 60;
+		s.math = 76;
+		System.out.println(" :"+s.name);
+		System.out.println(" :"+s.getTotal());
+		System.out.println(" :"+s.getAverage());
+	}
+}
+
+class Student {
+	String name;
+	int ban;
+	int no;
+	int kor;
+	int eng;
+	int math;
+	int getTotal() {
+		return kor + eng + math;
+	}
+	float getAverage() {
+		return (int)(getTotal() / 3f * 10 + 0.5f) / 10f;
+	}
+}
