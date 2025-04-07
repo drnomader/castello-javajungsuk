e6abfc22d70104af6216450e88816458ff70026a
drnomader <drnomader@gmail.com>
2025-04-07 21:54:08 +0900

    fix: 6_24

--- src/ch6/Exercise6_24.java
+++ src/ch6/Exercise6_24.java
@@ -1,15 +1,14 @@
 package ch6;
 
-class Exercise6_24
-{
+class Exercise6_24 {
 	public static int abs(int value) {
-		return value >=0 ? value : -value;
+		return value >= 0 ? value : -value;
 	}
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		int value = 5;
-		System.out.println(value+" :"+abs(value));
+		System.out.println(value + " : " + abs(value));
 		value = -10;
-		System.out.println(value+" :"+abs(value));
+		System.out.println(value + " : " + abs(value));
 	}
 }
