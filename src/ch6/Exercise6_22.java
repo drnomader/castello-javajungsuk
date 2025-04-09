3b6a21721ed8591b30c83ce58cb9f521a72ed3e7
drnomader <drnomader@gmail.com>
2025-04-09 23:10:40 +0900

    fix: 6_22

--- src/ch6/Exercise6_22.java
+++ src/ch6/Exercise6_22.java
@@ -1,21 +1,24 @@
 package ch6;
 
 class Exercise6_22 {
-	public static boolean isNumber(String str) {
-		if(str==null || str.equals(""))
-			return false;
-		for(int i=0; i< str.length();i++) {
-			char ch = str.charAt(i);
-			if(ch < '0' || ch > '9') {
-				return false;
-			}
-		} // for
-		return true;
-	}
 	public static void main(String[] args) {
 		String str = "123";
-		System.out.println(str+" ? "+isNumber(str));
+		System.out.println(str + " ? " + isNumber(str));
 		str = "1234o";
-		System.out.println(str+" ? "+isNumber(str));
+		System.out.println(str + " ? " + isNumber(str));
+	}
+
+	public static boolean isNumber(String str) {
+		if (str == null || str.isEmpty()) {
+			return false;
+		}
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (ch < '0' || ch > '0') {
+				return false;
+			}
+		}
+		return true;
 	}
 }
