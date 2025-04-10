6dce1f56382c987aba4c0516bbbaa71226f968ba
drnomader <drnomader@gmail.com>
2025-04-10 22:05:13 +0900

    refactor: 6_21

--- src/ch6/Exercise6_21.java
+++ src/ch6/Exercise6_21.java
@@ -11,32 +11,27 @@ class MyTv {
 	final int MIN_CHANNEL = 1;
 
 	void turnOnOff() {
-		// (1) isPowerOn true false , false true . 의 값이 면 로 면 로 바꾼다
 		isPowerOn = !isPowerOn;
 	}
 	void volumeUp() {
-		// (2) volume MAX_VOLUME 1 . 의 값이 보다 작을 때만 값을 증가시킨다
-		if(volume < MAX_VOLUME)
+		if (volume < MAX_VOLUME) {
 			volume++;
+		}
 	}
 	void volumeDown() {
-		// (3) volume MIN_VOLUME 1 . 의 값이 보다 클 때만 값을 감소시킨다
-		if(volume > MIN_VOLUME)
+		if (volume > MIN_VOLUME) {
 			volume--;
+		}
 	}
 	void channelUp() {
-		// (4) channel 1 . 의 값을 증가시킨다
-		// channel MAX_CHANNEL , channel MIN_CHANNEL . 만일 이 이면 의 값을 로 바꾼다
-		if(channel==MAX_CHANNEL) {
+		if (channel == MAX_CHANNEL) {
 			channel = MIN_CHANNEL;
 		} else {
 			channel++;
 		}
 	}
 	void channelDown() {
-		// (5) channel 1 . 의 값을 감소시킨다
-		// channel MIN_CHANNEL , channel MAX_CHANNEL . 만일 이 이면 의 값을 로 바꾼다
-		if(channel==MIN_CHANNEL) {
+		if (channel == MIN_CHANNEL) {
 			channel = MAX_CHANNEL;
 		} else {
 			channel--;
@@ -50,15 +45,15 @@ class Exercise6_21 {
 
 		t.channel = 100;
 		t.volume = 0;
-		System.out.println("CH:"+t.channel+", VOL:"+ t.volume);
+		System.out.println("CH: " + t.channel + ", VOL :" + t.volume);
 
 		t.channelDown();
 		t.volumeDown();
-		System.out.println("CH:"+t.channel+", VOL:"+ t.volume);
+		System.out.println("CH: " + t.channel + ", VOL :" + t.volume);
 
 		t.volume = 100;
 		t.channelUp();
 		t.volumeUp();
-		System.out.println("CH:"+t.channel+", VOL:"+ t.volume);
+		System.out.println("CH: " + t.channel + ", VOL :" + t.volume);
 	}
 }
