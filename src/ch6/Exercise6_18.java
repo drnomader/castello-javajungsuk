c5f9d16a527394b068b00f6ee000670afcf65948
drnomader <drnomader@gmail.com>
2025-04-13 23:06:42 +0900

    refactor: 6_18

--- src/ch6/Exercise6_18.java
+++ src/ch6/Exercise6_18.java
@@ -5,23 +5,28 @@ public class Exercise6_18 {
 
 class MemberCall {
 	int iv = 10;
-	static int cv = 20;
 	int iv2 = cv;
-	//static int cv2 = iv; // A - 라인 컴파일 에러
+
+	static int cv = 20;
+	//static int cv2 = iv; // 컴파일 에러
+
 	static void staticMethod1() {
 		System.out.println(cv);
-		//System.out.println(iv); // B - 라인 컴파일 에러
+		//System.out.println(iv); // 컴파일 에러
 	}
-	void instanceMethod1() {
-		System.out.println(cv);
-		System.out.println(iv); // C 라인
-	}
+
 	static void staticMethod2() {
 		staticMethod1();
-		//instanceMethod1(); // D - 라인 컴파일 에러
+		//instanceMethod1(); // 컴파일 에러
 	}
+
+	void instanceMethod1() {
+		System.out.println(cv);
+		System.out.println(iv);
+	}
+
 	void instanceMethod2() {
-		staticMethod1(); // E 라인
+		staticMethod1();
 		instanceMethod1();
 	}
 }
