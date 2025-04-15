cfa830b4fb1b11e116c82b0014f72862bec5eeb4
drnomader <drnomader@gmail.com>
2025-04-15 21:37:42 +0900

    refactor: 6_8

--- src/ch6/Exercise6_8.java
+++ src/ch6/Exercise6_8.java
@@ -4,14 +4,18 @@ public class Exercise6_8 {
 }
 
 class PlayingCard {
-	int kind;
-	int num;
+
 	static int width;
 	static int height;
+
+	int kind;
+	int num;
+
 	PlayingCard(int k, int n) {
 		kind = k;
 		num = n;
 	}
+
 	public static void main(String args[]) {
 		PlayingCard card = new PlayingCard(1,1);
 	}
