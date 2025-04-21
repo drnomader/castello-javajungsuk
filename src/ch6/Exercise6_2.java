772954192d968194e482c2db2e881c35013d36ef
drnomader <drnomader@gmail.com>
2025-04-21 23:23:19 +0900

    refactor: 6_2

--- src/ch6/Exercise6_2.java
+++ src/ch6/Exercise6_2.java
@@ -4,8 +4,8 @@ public class Exercise6_2 {
 	public static void main(String args[]) {
 		SutdaCard6_2 card1 = new SutdaCard6_2(3, false);
 		SutdaCard6_2 card2 = new SutdaCard6_2();
-		System.out.println(card1.info()); // 3 . 이 출력된다
-		System.out.println(card2.info()); // 1K . 가 출력된다
+		System.out.println(card1.info());
+		System.out.println(card2.info());
 	}
 }
 
@@ -14,7 +14,7 @@ class SutdaCard6_2 {
 	boolean isKwang;
 
 	SutdaCard6_2() {
-		this(1, true); // SutdaCard(1, true) . 를 호출한다
+		this(1, true);
 	}
 
 	SutdaCard6_2(int num, boolean isKwang) {
@@ -22,7 +22,7 @@ class SutdaCard6_2 {
 		this.isKwang = isKwang;
 	}
 
-	String info() { // . ( ) K . 숫자를 문자열로 반환한다 광 인 경우 를 덧붙인다 光
+	String info() {
 		return num + (isKwang ? "K" : "");
 	}
 }
