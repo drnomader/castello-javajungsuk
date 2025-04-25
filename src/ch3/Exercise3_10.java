5aed19957f46528165bb6b196cc7bed2648d6ab9
drnomader <drnomader@gmail.com>
2025-04-25 23:20:14 +0900

    refactor: 3_10

--- src/ch3/Exercise3_10.java
+++ src/ch3/Exercise3_10.java
@@ -3,7 +3,6 @@ package ch3;
 public class Exercise3_10 {
 	public static void main(String[] args) {
 		char ch = 'A';
-
 		char lowerCase = ('A' <= ch && ch <= 'z') ? (char) (ch + 32) : ch;
 
 		System.out.println("ch = " + ch);
