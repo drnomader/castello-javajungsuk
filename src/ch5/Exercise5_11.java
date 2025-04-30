19edeb9537579b35063fce8d3f4b3ec4e1802145
drnomader <drnomader@gmail.com>
2025-04-30 21:52:44 +0900

    refactor: 5_11

--- src/ch5/Exercise5_11.java
+++ src/ch5/Exercise5_11.java
@@ -10,10 +10,10 @@ public class Exercise5_11 {
 				, {40, 40, 40}
 				, {50, 50, 50}
 		};
-		int[][] result = new int[score.length+1][score[0].length+1];
+		int[][] result = new int[score.length + 1][score[0].length + 1];
 
-		for(int i=0; i < score.length;i++) {
-			for(int j=0; j < score[i].length;j++) {
+		for(int i = 0; i < score.length; i++) {
+			for(int j = 0; j < score[i].length; j++) {
 				result[i][j] = score[i][j];
 				result[i][score[0].length] += result[i][j];
 				result[score.length][j] += result[i][j];
@@ -21,11 +21,11 @@ public class Exercise5_11 {
 			}
 		}
 
-		for(int i=0; i < result.length;i++) {
-			for(int j=0; j < result[i].length;j++) {
-				System.out.printf("%4d",result[i][j]);
+		for(int i = 0; i < result.length; i++) {
+			for(int j = 0; j < result[i].length; j++) {
+				System.out.printf("%4d", result[i][j]);
 			}
 			System.out.println();
 		}
-	} // main
+	}
 }
