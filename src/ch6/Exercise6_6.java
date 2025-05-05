1ed8f03ed0c3345558d3dc25e950011ca615b6c0
drnomader <drnomader@gmail.com>
2025-05-05 23:11:32 +0900

    refactor: 6_6

--- src/ch6/Exercise6_6.java
+++ src/ch6/Exercise6_6.java
@@ -5,8 +5,7 @@ public class Exercise6_6 {
 		System.out.println(getDistance(1,1,2,2));
 	}
 
-	// (x,y) (x1,y1) 두 점 와 간의 거리 구하기
 	static double getDistance(int x, int y, int x1, int y1) {
-		return Math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1)); // x, y는 지역변수
+		return Math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
 	}
 }
