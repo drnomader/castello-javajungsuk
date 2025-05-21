f499327390e71608190730cc10602e1a4d9a35d6
drnomader <drnomader@gmail.com>
2025-05-21 23:08:37 +0900

    refactor: 6_23

--- src/ch6/Exercise6_23.java
+++ src/ch6/Exercise6_23.java
@@ -6,15 +6,15 @@ class Exercise6_23{
 		System.out.println(java.util.Arrays.toString(data));
 		System.out.println(" : " + max(data));
 		System.out.println(" : " + max(null));
-		System.out.println(" : " + max(new int[]{})); // 최대값 크기가 0인 배열
+		System.out.println(" : " + max(new int[]{}));
 	}
 
 	public static int max(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return -999999;
 		}
-		int max = arr[0]; // 배열의 첫 번째 값으로 최대값을 초기화
-		for (int i = 1; i < arr.length; i++) { // 배열의 두 번째 값부터 비교
+		int max = arr[0];
+		for (int i = 1; i < arr.length; i++) {
 			if (arr[i] > max)
 				max = arr[i];
 		}
