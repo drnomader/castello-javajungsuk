5aa92eab1482d9c9d24831d47e21b262666301fe
drnomader <drnomader@gmail.com>
2025-05-22 23:56:45 +0900

    refactor: 6_23

--- src/ch6/Exercise6_23.java
+++ src/ch6/Exercise6_23.java
@@ -13,11 +13,13 @@ class Exercise6_23{
 		if (arr == null || arr.length == 0) {
 			return -999999;
 		}
+
 		int max = arr[0];
 		for (int i = 1; i < arr.length; i++) {
 			if (arr[i] > max)
 				max = arr[i];
 		}
+
 		return max;
 	}
 }
