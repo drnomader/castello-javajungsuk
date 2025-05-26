591da40df312a8ef75e8b3cd41279c4986d052c0
drnomader <drnomader@gmail.com>
2025-05-26 21:36:48 +0900

    refactor: 6_23

--- src/ch6/Exercise6_23.java
+++ src/ch6/Exercise6_23.java
@@ -3,6 +3,7 @@ package ch6;
 class Exercise6_23{
 	public static void main(String[] args) {
 		int[] data = {3, 2, 9, 4, 7};
+
 		System.out.println(java.util.Arrays.toString(data));
 		System.out.println(" : " + max(data));
 		System.out.println(" : " + max(null));
